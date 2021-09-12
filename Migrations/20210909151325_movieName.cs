e4f2292964d4bff12a005c4900312775e1647a14
balbachl <balbachl@nmc.edu>
2021-09-12 15:51:09 -0400

    Add project files.

---
+++ Migrations/20210909151325_movieName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class movieName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoName",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoName",
+                table: "Movie");
+        }
+    }
+}
